df599695d3bd5dd7380c6aabd1d8c27892cc692b
saitenntaisei <saitenntaisei@gmail.com>
2023-11-23 01:49:46 +0900

    test

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -65,7 +65,6 @@ type Isu struct {
 	ID         int       `db:"id" json:"id"`
 	JIAIsuUUID string    `db:"jia_isu_uuid" json:"jia_isu_uuid"`
 	Name       string    `db:"name" json:"name"`
-	Image      []byte    `db:"image" json:"-"`
 	Character  string    `db:"character" json:"character"`
 	JIAUserID  string    `db:"jia_user_id" json:"-"`
 	CreatedAt  time.Time `db:"created_at" json:"-"`
