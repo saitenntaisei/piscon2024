916812d9c3b576fd23a4fdd0149dc81099d1a57b
saitenntaisei <saitenntaisei@gmail.com>
2023-11-22 23:11:47 +0900

    delete logger

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -215,10 +215,10 @@ func main() {
 		log.Fatal(http.ListenAndServe(":6060", nil))
 	}()
 	e := echo.New()
-	e.Debug = true
-	e.Logger.SetLevel(log.DEBUG)
+	// e.Debug = true
+	// e.Logger.SetLevel(log.DEBUG)
 
-	e.Use(middleware.Logger())
+	// e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
 	e.POST("/initialize", postInitialize)
